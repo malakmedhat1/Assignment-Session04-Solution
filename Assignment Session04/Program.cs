232dc2c44914221c87500bfef038439ea33f9189
malakmedhat1 <malakmedhat41@gmail.com>
2025-07-14 21:02:33 +0300

    Add project files.

---
+++ Assignment Session04/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Assignment_Session04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 - Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            /// Example:
+            /// Input: 5
+            /// Output: 1, 2, 3, 4, 5
+
+            #endregion
+
+            #region Q2 - Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+            /// Example:
+            /// Input: 5
+            /// Output: 5 10 15 20 25 30 35 40 45 50 55 60
+
+            #endregion
+
+            #region Q3 - Write a program that allows the user to insert a number then print all even numbers between 1 to this number.
+            /// Example:
+            /// Input: 15
+            /// Output: 2 4 6 8 10 12 14
+
+            #endregion
+
+            #region Q4 - Write a program that takes two integers then prints the power.
+            /// Example:
+            /// Input: 4 3
+            /// Output: 64
+            /// Hint: how to calculate 4^3 = 4 * 4 * 4 = 64
+
+            #endregion
+
+            #region Q5 - Write a program to enter marks of five subjects and calculate total, average and percentage.
+            /// Example:
+            /// Input: 95 76 58 90 89
+            /// Output: 
+            /// Total marks = 408
+            /// Average Marks = 81
+            /// Percentage = 81
+
+            #endregion
+
+            #region Q6 - Write a program to allow the user to enter a string and print the REVERSE of it.
+
+            #endregion
+
+            #region Q7 - Write a program to allow the user to enter an int and print the REVERSED of it.
+
+            #endregion
+
+            #region Q8 - Write a program to find prime numbers within a range of numbers.
+            /// Test Data:
+            /// Input starting number of range: 1
+            /// Input ending number of range: 50
+            /// Expected Output:
+            /// The prime numbers between 1 and 50 are:
+            /// 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+
+            #endregion
+
+            #region Q9 - Write a program to convert a decimal number into binary without using an array.
+            /// Test Data:
+            /// Input: 25
+            /// Output: The Binary of 25 is 11001
+
+            #endregion
+
+            #region Q10 - Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3) and determines whether these points lie on a single straight line.
+
+            #endregion
+
+            #region Q11 - Write a program that prints an identity matrix using a for loop.
+            /// The program takes a value n from the user and shows the identity table of size n * n.
+
+            #endregion
+        }
+    }
+}
